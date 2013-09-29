d66760884a9134266b4f15bf4bbbb484d22bcf93
Victor Piousbox <piousbox@gmail.com>
2013-09-29 16:09:31 -0700

    tiny typo

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -14,7 +14,7 @@ describe ReportsController do
     get :show, :name_seo => @report.name_seo
     response.should be_success
     assert_select( '.report-name-seo', 1 )
-    assert_delect( '.report-show-hidden .descr', 1 )
+    assert_select( '.report-show-hidden .descr', 1 )
   end
 
 end
