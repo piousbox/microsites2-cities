4e4882cd06ba3aa3450fdb2eddb1eec88c5355f5
Victor Piousbox <piousbox@gmail.com>
2013-11-17 15:25:41 -0800

    better compile assets

--- config/application.rb
+++ config/application.rb
@@ -48,8 +48,8 @@ module Microsites2
     config.filter_parameters += [ :password, :password_confirmation, :password_confirm ]
 
     config.assets.paths << Rails.root.join("app", "assets", "flash")
-    config.assets.precompile += [ 'cities.css', 'application.css', 'lightbox.css' ]
-    config.assets.precompile += [ 'rails.js', 'application.js' ]
+    config.assets.precompile += [ 'cities.css', 'application.css', 'lightbox.css', 'reset-min' ]
+    config.assets.precompile += [ 'rails.js', 'application.js', 'bootstrap.js', 'global.js', 'lightbox.js' ]
 
     # Use SQL instead of Active Record's schema dumper when creating the database.
     # This is necessary if your schema can't be completely dumped by the schema dumper,
