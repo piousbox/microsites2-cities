64ca2c984752bc64543a1e4a474e396454fdbd45
Victor Piousbox <piousbox@gmail.com>
2014-01-10 09:04:11 -0800

    removed lib_trash/

--- lib-trash/README.md
+++
@@ -1,2 +0,0 @@
-
-This repo contains the tools and tasks (the library) of Pi, M2C, and (most importantly) the Mobi guide.
--- lib-trash/test_extensions.rb
+++
@@ -1,9 +0,0 @@
-
-
-
-
-def create_users
-  User.all.each { |u| u.remove }
-  FactoryGirl.create :user
-  FactoryGirl.create :piousbox
-end
