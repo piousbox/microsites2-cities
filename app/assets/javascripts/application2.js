e7f450dbf33da41dfd7e637158d6b0438029ddd2
Victor Piousbox <piousbox@gmail.com>
2014-01-10 09:34:21 -0800

    moving javascript to javascripts_trash/

---
+++ app/assets/javascripts/application2.js
@@ -0,0 +1,31 @@
+// 
+//
+//= require jquery-1.7.1
+// require jquery_ujs
+// require jquery-ui.min
+//
+// require tinymce
+// require tinymce/langs/en
+// require tinymce/themes/advanced/editor_template
+//
+// require lightbox
+//
+//= require foundation/foundation
+//= require foundation/foundation.tab
+//= require foundation/foundation.topbar
+// 
+//= require angular/angular
+//= require angular/angular-route
+//= require angular/angular-resource
+//= require angular/angular-sanitize
+// 
+//= require ./app
+//= require ./services
+//= require ./controllers
+//= require ./filters
+//= require ./resources
+//= require ./directives
+//
+
+$(document).foundation();
+CanvasOps = {};
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -25,6 +25,7 @@ class WelcomeController < ApplicationController
   end
 
   def home
+    render :layout => 'cities2'
     # redirect_to cities_path
   end
 
